18107d190f409e6f61dc7e19714e4b90323176d2
obarisk <obarisk@gmail.com>
2025-09-09 08:42:20 +0800

    fix unittest

--- providers/google/tests/unit/google/cloud/hooks/test_bigquery.py
+++ providers/google/tests/unit/google/cloud/hooks/test_bigquery.py
@@ -681,22 +681,20 @@ class TestBigQueryHookMethods(_BigQueryBaseTestClass):
         )
         assert job_id == expected_job_id
 
-    def test_get_exec_date(self):
+    def test_get_run_after_or_logical_date(self):
         import pendulum
 
         if AIRFLOW_V_3_0_PLUS:
             from airflow.models import DagRun
             from airflow.sdk.definitions.context import Context
 
-            ctx = Context(logical_date=pendulum.datetime(2025, 1, 1))
-            assert self.hook.get_exec_date(ctx) == pendulum.datetime(2025, 1, 1)
             ctx = Context(dag_run=DagRun(run_after=pendulum.datetime(2025, 1, 1)))
-            assert self.hook.get_exec_date(ctx) == pendulum.datetime(2025, 1, 1)
+            assert self.hook.get_run_after_or_logical_date(ctx) == pendulum.datetime(2025, 1, 1)
         else:
             from airflow.utils.context import Context
 
             ctx = Context(logical_date=pendulum.datetime(2025, 1, 1))
-            assert self.hook.get_exec_date(ctx) == pendulum.datetime(2025, 1, 1)
+            assert self.hook.get_run_after_or_logical_date(ctx) == pendulum.datetime(2025, 1, 1)
 
     @mock.patch(
         "airflow.providers.google.cloud.hooks.bigquery.BigQueryHook.get_job",
